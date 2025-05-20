ad4665ef3bdbb0528c1439c8cfc778359f6436e7
Pilar Gallego <pgsubias@gmail.com>
2025-05-20 14:41:54 +0000

    cambiar idioma, crear trip panel

--- front/src/components/NavBar.jsx
+++ front/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Planificador</a>
+        <a className="navbar-brand" href="#">Planner</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
           data-bs-target="#navbarNav" aria-controls="navbarNav"
           aria-expanded="false" aria-label="Toggle navigation">
@@ -11,13 +11,13 @@ function NavBar() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link active" href="/">Inicio</a>
+              <a className="nav-link active" href="/">Home</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/login">Iniciar sesión</a>
+              <a className="nav-link" href="/login">Login</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/register">Registrarse</a>
+              <a className="nav-link" href="/register">Sign in</a>
             </li>
           </ul>
         </div>
