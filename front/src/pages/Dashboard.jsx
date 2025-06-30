b84a64ea457c10c6ba2997ee5af708628aa8c97a
Pilar Gallego <pgsubias@gmail.com>
2025-06-30 16:50:46 +0000

    añadir start en el pipenv, handle newtrip y el estilo para el dashboard

--- front/src/pages/Dashboard.jsx
+++ front/src/pages/Dashboard.jsx
@@ -2,8 +2,11 @@ import { useState, useEffect } from "react";
 import TripCard from "../components/TripCard.jsx";
 import NewTripForm from "../components/NewTripForm.jsx";
 
+import '../styles/Dashboard.css'
+
 export const Dashboard = () => {
   const [trips, setTrips] = useState([]);
+  const [showForm, setShowForm] = useState(false);
   const [loadingId, setLoadingId] = useState(null); // Para saber qué viaje está borrando
 
   useEffect(() => {
@@ -12,6 +15,10 @@ export const Dashboard = () => {
       .then((data) => setTrips(data))
       .catch((error) => console.error("Error al cargar los viajes:", error));
   }, []);
+
+  const handleNewTrip = (NewTripForm)=> {
+    setTrips((prevTrips)=>[...prevTrips, NewTripForm])
+  }
 
   const handleDelete = (id) => {
     setLoadingId(id); // Marcar el viaje como "en proceso de borrado"
@@ -34,19 +41,35 @@ export const Dashboard = () => {
   return (
     <div>
       <h2>Mis Viajes</h2>
-      <NewTripForm onNewTrip={handleNewTrip} />
+      <button onClick={()=> setShowForm(true)}>Añadir viaje</button>
 
-      {trips.map((trip) => (
-        <TripCard
-          key={trip.id}
-          id={trip.id}
-          destination={trip.destination}
-          startDate={trip.startDate}
-          endDate={trip.endDate}
-          onDelete={handleDelete}
-          isDeleting={trip.id === loadingId}
-        />
-      ))}
+      <div className="dashboard-container">
+        <div className="trip-list">
+          {trips.map((trip) => (
+            <TripCard
+              key={trip.id}
+              id={trip.id}
+              destination={trip.destination}
+              startDate={trip.startDate}
+              endDate={trip.endDate}
+              onDelete={handleDelete}
+              isDeleting={trip.id === loadingId}
+            />
+          ))}
+        </div>
+
+        {showForm && (
+          <div className="form-panel">
+            <NewTripForm
+              onNewTrip={(newTrip) => {
+                setTrips((prev) => [...prev, newTrip]);
+                setShowForm(false);
+              }}
+              onCancel={() => setShowForm(false)}
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
